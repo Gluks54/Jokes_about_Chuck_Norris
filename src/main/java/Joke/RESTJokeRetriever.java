bcee28eeeb2bb9aa825984d5d0619cc9cd62b900
Yahor Viarenich <gluks0575@gmail.com>
2018-12-29 15:12:37 +0100

    add RESTJokeRetriver Test and method

--- src/main/java/Joke/RESTJokeRetriever.java
+++ src/main/java/Joke/RESTJokeRetriever.java
@@ -1,11 +1,18 @@
 package Joke;
 
 import History.Joke;
+import com.google.gson.Gson;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class RESTJokeRetriever implements JokeRetriever {
 
     @Override
-    public Joke getJoke() {
-        return null;
+    public Joke getJoke() throws UnirestException {
+        String url = "https://api.chucknorris.io/jokes/random";
+        Gson gson = new Gson();
+        String responce = Unirest.get(url).asString().getBody();
+        Joke joke = gson.fromJson(responce,Joke.class);
+        return joke;
     }
 }
--- src/test/java/RESTJokeRetrieverTest.java
+++ src/test/java/RESTJokeRetrieverTest.java
@@ -1,2 +1,19 @@
+import History.Joke;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.junit.Before;
+import org.junit.Test;
+import Joke.*;
+import static junit.framework.TestCase.assertTrue;
+
 public class RESTJokeRetrieverTest {
+    RESTJokeRetriever restJokeRetriever;
+    @Before
+    public void setUp(){
+        restJokeRetriever = new RESTJokeRetriever();
+    }
+
+    @Test
+    public void testRESTJokeRetriver() throws UnirestException {
+        assertTrue(restJokeRetriever.getJoke() != null);
+    }
 }
